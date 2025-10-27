3b48e0f254d2a4f5ae86b86c3f2f16af491942a5
Ritesh Bhoskar <riteshbhoskar7@gmail.com>
2025-10-27 13:47:29 +0530

    basic http server

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/RiteshBhoskar/mealbook-backend/internal/db"
 )
 
 func main() {
-	db.Connect()
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Mealbook backend running!")
 	})
