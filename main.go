e56a7bdd627348c3521678dc64669a2e082be09f
Ritesh Bhoskar <riteshbhoskar7@gmail.com>
2025-10-27 12:53:02 +0530

    testing the cd pipline

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/RiteshBhoskar/mealbook-backend/internal/db"
 )
 
 func main() {
 	db.Connect()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Mealbook backend running!")
+	})
 }
